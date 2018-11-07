55dd96c531795f300697b3f85f5b510f278d8b68
dev_intern3 <dev_intern3@17-005-02mm.local>
2018-11-07 11:16:08 +0900

    Initial Commit

---
+++ 実践課題２/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  実践課題２
+//
+//  Created by dev室 インターン３ on 2018/11/07.
+//  Copyright © 2018年 dev室 インターン３. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
